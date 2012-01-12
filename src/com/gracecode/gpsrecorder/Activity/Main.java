7ea1bea65efb838c52c9f871fb45eed85ef927b6
mingcheng <lucky@gracecode.com>
2012-01-12 15:14:33 +0800

    refactor the code

--- src/com/gracecode/gpsrecorder/Activity/Main.java
+++ src/com/gracecode/gpsrecorder/Activity/Main.java
@@ -26,6 +26,10 @@ public class Main extends BaseActivity {
     private Timer timer;
     private static double maxSpeed = 0.0;
 
+    private ArrayList<TextView> textViewsGroup = new ArrayList<TextView>();
+    private LocationItem lastLocationRecord;
+    private static final int MESSAGE_UPDATE_STATE_VIEW = 0x0001;
+
     /**
      * Handle the records for show the last recorded status.
      */
@@ -60,18 +64,12 @@ public class Main extends BaseActivity {
         }
     };
 
-
-    private ArrayList<TextView> textViewsGroup = new ArrayList<TextView>();
-    private LocationItem lastLocationRecord;
-    private static final int MESSAGE_UPDATE_STATE_VIEW = 0x0001;
-
-
     /**
      * 找到所有的 TextView 元素
      *
      * @param v
      */
-    public void findAllTextView(ViewGroup v) {
+    private void findAllTextView(ViewGroup v) {
         for (int i = 0; i < v.getChildCount(); i++) {
             View item = v.getChildAt(i);
             if (item instanceof TextView) {
@@ -81,22 +79,6 @@ public class Main extends BaseActivity {
             }
         }
     }
-
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-
-        if (!Environment.isExternalStoragePresent()) {
-            Log.e(TAG, "External storage not presented.");
-            return;
-        }
-
-        findAllTextView((ViewGroup) findViewById(R.id.root));
-        initialViewUpdater();
-    }
-
 
     private void initialViewUpdater() {
         timer = new Timer();
@@ -118,6 +100,103 @@ public class Main extends BaseActivity {
         }
     }
 
+    private void updateView() {
+        // get last record by server binder
+        lastLocationRecord = serviceBinder.getLastRecord();
+
+        for (int i = 0; i < textViewsGroup.size(); i++) {
+            double numberValue = 0;
+            String stringValue = "";
+            TextView textView = textViewsGroup.get(i);
+            int count = lastLocationRecord.getCount();
+
+            try {
+                switch (textView.getId()) {
+                    case R.id.status:
+                        int stamp = (serviceBinder.getStatus() == ServiceBinder.STATUS_RUNNING) ? R.string.recording : R.string.idle;
+                        stringValue = getString(stamp);
+                        break;
+                    case R.id.records:
+                        if (count > 0) {
+                            stringValue = String.format(getString(R.string.records), count);
+                        } else {
+                            stringValue = getString(R.string.norecords);
+                        }
+                        break;
+                    case R.id.time:
+                        stringValue = new SimpleDateFormat(getString(R.string.time_format))
+                            .format(new Date(lastLocationRecord.getTime()));
+                        break;
+                    case R.id.speed:
+                        double speed = lastLocationRecord.getSpeed();
+                        if (maxSpeed < speed) {
+                            maxSpeed = speed;
+                        }
+
+                        if (maxSpeed != 0.0) {
+                            stringValue = String.format("%.1f(%.1f)", speed, maxSpeed);
+                        } else {
+                            throw new NullPointerException();
+                        }
+                        break;
+                    case R.id.longitude:
+                        numberValue = lastLocationRecord.getLongitude();
+                        break;
+                    case R.id.latitude:
+                        numberValue = lastLocationRecord.getLatitude();
+                        break;
+                    case R.id.bearing:
+                        numberValue = lastLocationRecord.getBearing();
+                        break;
+                    case R.id.altitude:
+                        numberValue = lastLocationRecord.getAltitude();
+                        break;
+                    case R.id.accuracy:
+//                        numberValue = lastLocationRecord.getAccuracy();
+                        stringValue = String.format("%sm/%ds",
+                            sharedPreferences.getString(Environment.PREF_GPS_MINDISTANCE, Environment.DEFAULT_GPS_MINDISTANCE),
+                            Integer.parseInt(sharedPreferences.getString(Environment.PREF_GPS_MINTIME, Environment.DEFAULT_GPS_MINTIME)) / 1000);
+                        break;
+                }
+            } catch (NullPointerException e) {
+                stringValue = getString(R.string.norecords);
+            }
+
+            if (stringValue.length() > 0) {
+                textView.setText(stringValue);
+            } else if (numberValue != 0) {
+                textView.setText(String.format("%.2f", numberValue));
+            }
+        }
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        MenuItem pauseMenuItem = menu.findItem(R.id.pause);
+        MenuItem startMenuItem = menu.findItem(R.id.start);
+        Boolean isRunning = (ServiceBinder.STATUS_RUNNING == serviceBinder.getStatus());
+
+        pauseMenuItem.setEnabled(isRunning ? true : false);
+        startMenuItem.setEnabled(isRunning ? false : true);
+
+        return true;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        if (!Environment.isExternalStoragePresent()) {
+            Log.e(TAG, "External storage not presented.");
+            return;
+        }
+
+        findAllTextView((ViewGroup) findViewById(R.id.root));
+        initialViewUpdater();
+    }
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
@@ -126,11 +205,20 @@ public class Main extends BaseActivity {
         return true;
     }
 
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         Intent t;
 
         switch (item.getItemId()) {
+            case R.id.start:
+                serviceBinder.startRecord();
+                return true;
+
+            case R.id.pause:
+                serviceBinder.stopRecord();
+                return true;
+
             case R.id.stop:
                 serviceBinder.stopRecord();
                 stopService();
@@ -159,73 +247,6 @@ public class Main extends BaseActivity {
         }
     }
 
-    private void updateView() {
-        // get last record by server binder
-        lastLocationRecord = serviceBinder.getLastRecord();
-
-        for (int i = 0; i < textViewsGroup.size(); i++) {
-            double numberValue = 0;
-            String stringValue = "";
-            TextView textView = textViewsGroup.get(i);
-            int count = lastLocationRecord.getCount();
-
-            try {
-                switch (textView.getId()) {
-                    case R.id.status:
-                        if (count > 0) {
-                            stringValue = getString(R.string.recording);
-                        } else {
-                            stringValue = getString(R.string.initialing);
-                        }
-                        break;
-                    case R.id.records:
-                        if (count > 0) {
-                            stringValue = String.format(getString(R.string.records), count);
-                        } else {
-                            stringValue = getString(R.string.norecords);
-                        }
-                        break;
-                    case R.id.time:
-                        stringValue = new SimpleDateFormat(getString(R.string.time_format))
-                            .format(new Date(lastLocationRecord.getTime()));
-                        break;
-                    case R.id.speed:
-                        double speed = lastLocationRecord.getSpeed();
-                        if (maxSpeed < speed) {
-                            maxSpeed = speed;
-                        }
-
-                        if (maxSpeed != 0.0) {
-                            stringValue = String.format("%.1f(%.1f)", speed, maxSpeed);
-                        }
-                        break;
-                    case R.id.longitude:
-                        numberValue = lastLocationRecord.getLongitude();
-                        break;
-                    case R.id.latitude:
-                        numberValue = lastLocationRecord.getLatitude();
-                        break;
-                    case R.id.bearing:
-                        numberValue = lastLocationRecord.getBearing();
-                        break;
-                    case R.id.altitude:
-                        numberValue = lastLocationRecord.getAltitude();
-                        break;
-                    case R.id.accuracy:
-                        numberValue = lastLocationRecord.getAccuracy();
-                        break;
-                }
-            } catch (NullPointerException e) {
-                stringValue = getString(R.string.norecords);
-            }
-
-            if (stringValue.length() > 0) {
-                textView.setText(stringValue);
-            } else if (numberValue != 0) {
-                textView.setText(String.format("%.2f", numberValue));
-            }
-        }
-    }
 
     @Override
     public void onDestroy() {
