bbb52c8c6aff01d7971670117cd06e75c8646df9
mingcheng <lucky@gracecode.com>
2012-04-23 10:17:31 +0800

    update some changes

---
+++ src/com/gracecode/gpsrecorder/Util/Log.java
@@ -0,0 +1,11 @@
+package com.gracecode.gpsrecorder.util;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: mingcheng
+ * Date: 12-4-7
+ * Time: 下午2:52
+ * To change this template use File | Settings | File Templates.
+ */
+public class Log {
+}
